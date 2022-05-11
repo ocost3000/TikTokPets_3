57c6d21d33fdc577f5b6cd71d34c99909970cd8f
Omar Costilla <omarcostilla6@gmail.com>
2022-05-11 16:00:46 -0700

    random videos chosen in compare.js

--- public/compare.js
+++ public/compare.js
@@ -10,15 +10,22 @@ for (let i=0; i<2; i++) {
 
 // hard-code videos for now
 // You will need to get pairs of videos from the server to play the game.
-const urls = ["https://www.tiktok.com/@berdievgabinii/video/7040757252332047662",
-"https://www.tiktok.com/@catcatbiubiubiu/video/6990180291545468166"];
+// const urls = ["https://www.tiktok.com/@berdievgabinii/video/7040757252332047662",
+// "https://www.tiktok.com/@catcatbiubiubiu/video/6990180291545468166"];
+// TODO: Make dynamic
 
-for (let i=0; i<2; i++) {
+let urls = []
+sendGetRequest("/getTwoVideos")
+  .then(res => {
+    console.log(res)
+    urls = res.map(x => x.url)
+    console.log(urls)
+    for (let i=0; i<2; i++) {
       addVideo(urls[i],videoElmts[i]);
     }
     // load the videos after the names are pasted in! 
     loadTheVideos();
-
-
-
-    
+  })
+  .catch(err => {
+    console.log(err)
+  });
