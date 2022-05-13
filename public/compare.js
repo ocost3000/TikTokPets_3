7bd9a119b5125fcc58872bc7ccce9b56347db2d8
Omar Costilla <omarcostilla6@gmail.com>
2022-05-13 00:30:35 +0000

    basic logic for pt. 4, still need solid heart

--- public/compare.js
+++ public/compare.js
@@ -2,17 +2,20 @@ let videoElmts = document.getElementsByClassName("tiktokDiv");
 
 let reloadButtons = document.getElementsByClassName("reload");
 let heartButtons = document.querySelectorAll("div.heart");
+heartButtons[0].adversary = heartButtons[1]
+heartButtons[1].adversary = heartButtons[0]
 for (let i=0; i<2; i++) {
   let reload = reloadButtons[i]; 
   reload.addEventListener("click",function() { reloadVideo(videoElmts[i]) });
   heartButtons[i].classList.add("unloved");
+  heartButtons[i].addEventListener("click", event => {
+    // TODO: Make the preferred heart solid
+    console.log("Button Pressed")
+    console.log(event.currentTarget)
+    event.currentTarget.classList.remove("unloved")
+    event.currentTarget.adversary.classList.add("unloved")
+  });
 } // for loop
-
-// hard-code videos for now
-// You will need to get pairs of videos from the server to play the game.
-// const urls = ["https://www.tiktok.com/@berdievgabinii/video/7040757252332047662",
-// "https://www.tiktok.com/@catcatbiubiubiu/video/6990180291545468166"];
-// TODO: Make dynamic
 
 let urls = []
 sendGetRequest("/getTwoVideos")
